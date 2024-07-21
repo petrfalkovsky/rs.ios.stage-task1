3573c153bc508eb2fd3aa126363f8bfb58d5ee8c
petrfalkovsky <petrfalkovsky@yandex.ru>
2024-07-21 19:35:26 +0300

    feat: add resolve trackLeaderBoard

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,35 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+    // Обработка крайних случаев
+    if (playerArray.count == 0) return @[];
+    
+    
+    if (rankedArray.count == 0) {
+        NSMutableArray *allFirstPlace = [NSMutableArray arrayWithCapacity:playerArray.count];
+        for (NSInteger i = 0; i < playerArray.count; i++) {
+            [allFirstPlace addObject:@1];
+        }
+        return [allFirstPlace copy];
+    }
+
+    // Удаление дубликатов и сортировка в порядке убывания
+    NSOrderedSet *uniqueRankedSet = [NSOrderedSet orderedSetWithArray:rankedArray];
+    NSArray *uniqueRankedArray = [[uniqueRankedSet array] sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"self" ascending:NO]]];
+    
+    NSMutableArray *rankings = [NSMutableArray arrayWithCapacity:playerArray.count];
+    NSInteger rankedIndex = uniqueRankedArray.count - 1;
+    
+    for (NSNumber *playerScore in playerArray) {
+        // Проходим по рейтингу с конца (самого низкого) к началу (самого высокому)
+        while (rankedIndex >= 0 && playerScore.integerValue >= [uniqueRankedArray[rankedIndex] integerValue]) {
+            rankedIndex--;
+        }
+        // Ранг это (rankedIndex + 1) + 1, потому что ранг это индекс+1 и нам нужна следующая позиция
+        [rankings addObject:@(rankedIndex + 2)];
+    }
+    
+    return [rankings copy];
 }
 
 @end
