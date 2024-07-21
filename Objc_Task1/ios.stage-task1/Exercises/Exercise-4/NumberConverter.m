26cac2617295895f86c795e78897f388fa54f744
petrfalkovsky <petrfalkovsky@yandex.ru>
2024-07-21 19:35:38 +0300

    feat: add resolve numberConverter

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,21 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    if (number == nil) return @[];
+    
+    // Преобразуем число в строковое представление абсолютного значения
+    NSString *numberString = [[number stringValue]stringByReplacingOccurrencesOfString:@"-"withString:@""];
+    
+    // Создаем изменяемый массив для хранения перевернутых символов
+    NSMutableArray *reversedArray = [NSMutableArray arrayWithCapacity:numberString.length];
+    
+    // Проходим по строке в обратном порядке и добавляем каждый символ в массив
+    for (NSInteger i = numberString.length - 1; i >= 0; i--) {
+        NSString *charString = [NSString stringWithFormat:@"%c", [numberString characterAtIndex:i]];
+        [reversedArray addObject:charString];
+    }
+    
+    return [reversedArray copy];
 }
 
 @end
