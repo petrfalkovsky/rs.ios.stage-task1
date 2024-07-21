c8131fc2ed8158aecb78b09a82011af13aef7f74
petrfalkovsky <petrfalkovsky@yandex.ru>
2024-07-21 19:36:08 +0300

    feat: add resolve NSString+ValidNumber

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,18 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    if (self.length == 0 ) return false;
+    
+    // от проинициализированного значние i равным 0,
+    // итерируемся по длинные цикла до тех пор пока переменная i не будет меньше длинны строки self.length
+    for (NSInteger i = 0; i < self.length; i++) {
+          unichar character = [self characterAtIndex:i];
+          if (!isdigit(character)) {
+              return false;
+          }
+      }
+    
+    return true;
 }
 
 @end
