afc24c0ab493dd7a599024d93c8b5b5afcc42c6b
petrfalkovsky <petrfalkovsky@yandex.ru>
2024-07-21 19:35:53 +0300

    feat: add resolve NSArray+MinRotated

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,7 +3,27 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-    return nil;
+    
+    if (self.count == 0) 
+        return nil;
+    
+    // обозначаю границы поиска слева направво с начала масива от нуля и до конца self.count - 1
+    NSInteger left = 0;
+    NSInteger right = self.count - 1;
+    
+    
+    while (left < right) {
+        NSInteger mid = left + (right - left) / 2;
+        
+    if ([self[mid] integerValue] < [self[right] integerValue]) {
+            right = mid;
+        } else {
+            left = mid +1;
+        }
+    }
+    
+    return self[left];
 }
 
 @end
+
